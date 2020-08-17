cfb04cf31861fc8148ad35dfdb9b8bff8fc0216f
cpurificacao <carlos_dbdesktop@outlook.com>
2020-08-17 18:27:45 -0300

    feat: Definicao do model BillingCycle

---
+++ src/database/models/billingCycle.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require("mongoose");
+
+const creditSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: Number,
+    min: 1,
+    required: true,
+  },
+});
+
+const debitSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: String,
+    min: 1,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    uppercase: true,
+    enum: ["PAGO", "PENDENTE", "AGENDADO"],
+  },
+});
+
+const billingCycleSchema = new Schema({
+  month: {
+    type: Number,
+    min: 1,
+    max: 12,
+    required: true,
+  },
+  year: {
+    type: Number,
+    min: 1970,
+    max: 2100,
+    required: true,
+  },
+  credits: [creditSchema],
+  debits: [debitSchema],
+});
+
+module.exports = model("BillingCycle", billingCycleSchema);
