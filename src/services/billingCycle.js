05eae37625b3c873607e359d747a51797a52e8b8
cpurificacao <carlos_dbdesktop@outlook.com>
2020-08-18 20:48:10 -0300

    feat: Adiciona rota /count para retorno do total de registros

--- src/services/billingCycle.js
+++ src/services/billingCycle.js
@@ -3,4 +3,14 @@ const billingCycleModel = require("../database/models/billingCycle");
 billingCycleModel.methods(["get", "post", "put", "delete"]);
 billingCycleModel.updateOptions({ new: true, runValidators: true });
 
+billingCycleModel.route("count", (req, res, next) => {
+  billingCycleModel.count((err, data) => {
+    if (err) {
+      return res.status(500).json({ errors: err });
+    }
+
+    res.json({ count: data });
+  });
+});
+
 module.exports = billingCycleModel;
