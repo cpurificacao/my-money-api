d0d464590faa465214bb271d445e542479ed8929
cpurificacao <carlos_dbdesktop@outlook.com>
2020-08-18 21:36:05 -0300

    feat: Adiciona rota /summary para retorno do sumario de creditos e debitos

--- src/services/billingCycle.js
+++ src/services/billingCycle.js
@@ -6,11 +6,53 @@ billingCycleModel.updateOptions({ new: true, runValidators: true });
 billingCycleModel.route("count", (req, res, next) => {
   billingCycleModel.count((err, data) => {
     if (err) {
-      return res.status(500).json({ errors: err });
+      return res.status(500).json({ errors: [err] });
     }
 
     res.json({ count: data });
   });
+});
+
+billingCycleModel.route("summary", (req, res, next) => {
+  billingCycleModel.aggregate(
+    [
+      {
+        $project: {
+          totalCredit: {
+            $sum: "$credits.value",
+          },
+          totalDebit: {
+            $sum: "$debits.value",
+          },
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalCredit: {
+            $sum: "$totalCredit",
+          },
+          totalDebit: {
+            $sum: "$totalDebit",
+          },
+        },
+      },
+      {
+        $project: {
+          _id: false,
+          totalCredit: true,
+          totalDebit: true,
+        },
+      },
+    ],
+    (err, data) => {
+      if (err) {
+        return res.status(500).json({ errors: [err] });
+      }
+
+      res.json(data[0] || { totalCredit: 0, totalDebit: 0 });
+    }
+  );
 });
 
 module.exports = billingCycleModel;
