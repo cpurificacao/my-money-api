b1d7249c3974a7b045b4c80701400f8e87309aea
cpurificacao <carlos_dbdesktop@outlook.com>
2020-08-18 23:53:20 -0300

    fix: Altera a CORS Origin para * (todos)

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const options = {
-  origin: "192.168.200.5",
+  origin: "*",
   methods: ["POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   allowedHeaders: ["Origin", "X-Request-With", "Content-Type", "Accept"],
 };
